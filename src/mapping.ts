1d0c20d38d0bcb39e6d5822cbd5142c971e770ae
= <=>
2022-02-19 17:36:51 +0700

    commit token list

---
+++ src/mapping.ts
@@ -0,0 +1,13 @@
+import { NameUpdated } from '../generated/HelloWorld/HelloWorld'
+import { Name } from '../generated/schema'
+
+export function handleNameUpdated(event: NameUpdated): void {
+    let id = event.transaction.hash.toHex()
+    let name = Name.load(id)
+    if (name == null) {
+        name = new Name(id)
+    }
+    name.newName = event.params.newName
+    name.updater = event.params.updater
+    name.save()
+}
